9bfaf84083d87d8d7f9d12e7128036839bd63659
Richardore <113269430+Richardore@users.noreply.github.com>
2022-12-06 22:32:53 -0500

    index y detalles

--- app/routes/rutas.py
+++ app/routes/rutas.py
@@ -13,3 +13,15 @@ bp_nombres = Blueprint('bp_nombres', __name__)
 def index():
     morty = db.Personaje.find({}).sort("name", 1)
     return render_template("index.html", morty=morty)
+
+@bp_nombres.route("/datos/<int:id>")
+def datos(id):
+    detalles = db.Personaje.find({'id': id})
+    return render_template('detalles.html', detalles=detalles)
+
+
+@bp_nombres.route("/capítulo/<int:id>")
+def deta_cap(id):
+    captituloss = db.Capitulo.find({'id': id}).sort("name", 1)
+
+    return render_template('capitulos.html', captituloss=captituloss)
